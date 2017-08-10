3354e9c28439f80806f1a0d2a3405215af7ece07
Rob Marano <robmarano@gmail.com>
2017-08-10 19:47:32 -0400

    Updated Vagrant to do remote X windowing for matplotlib.pyplot

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,6 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
     config.vm.provision :shell, path: "bootstrap.sh"
     config.vm.provision "file", source: "course-1/session-5/.vimrc", destination: ".vimrc"
+  config.ssh.forward_x11 = true
 end
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install vim wget git zlib1g-dev --assume-yes
 apt-get install python3 --assume-yes
 apt-get install python3-venv --assume-yes
 apt-get install bpython --assume-yes
+apt-get install python3-tk --assume-yes
 
 # Pip3
 apt-get install python3-pip --assume-yes
@@ -14,14 +15,15 @@ pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -
 pip3 install pandas
 pip3 install numpy
 pip3 install xlrd
+pip3 install matplotlib
 
 
 export RUN_USER="ubuntu"
 export RUNUSER_PROG="/sbin/runuser"
 
 export COMMAND=$(cat <<-'HERE1a'
-    echo "\"echo \"in .profile\" >> $HOME/.profile\""
     echo "\"echo \"in .bashrc\" >> $HOME/.bashrc\""
+    echo "\"echo \"in .profile\" >> $HOME/.profile\""
 HERE1a
 )
 
