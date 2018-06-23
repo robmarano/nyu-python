b5f03df6efb2ad556d0c1c46e64c9fed1607a089
Marano <maranoro@8c8590ccea49.ant.amazon.com>
2018-06-23 16:06:50 -0400

    Added method in Daily_Power.py to return the power value in watts at a particular hour of day, in military time.

--- course-3/assignments/electric_power/piv/Daily_Power.py
+++ course-3/assignments/electric_power/piv/Daily_Power.py
@@ -13,6 +13,7 @@ import numpy as np
 PLACEHOLDER_DATE = '1/1/2000'
 PERIODS = 24
 FREQUENCY = '1H'
+TOLERANCE = 0.1
 
 
 class Daily_Power(object):
@@ -35,6 +36,22 @@ class Daily_Power(object):
 		logging.info('Initialized Daily_Power object')
 		pass
 
+	def power(self, time):
+		'''
+		Return the power value in Watts at given hour
+		'''
+		if (type(time) is int):
+			if (time in range(0,23)):
+				power_value = self.power_readings.at[time,'power']
+				return power_value
+			else:
+				err_msg = 'Hour value ({}) is out of range.'.format(time)
+				raise ValueError(err_msg)
+		else:
+				err_msg = 'Time value ({}) is not integer value of an hour.'.format(time)
+				raise TypeError(err_msg)			
+		pass
+
 	def get_power_config(self):
 		self.power_config = pd.read_csv(self.power_config_file)
 		#print(self.power_config)
@@ -42,28 +59,30 @@ class Daily_Power(object):
 
 	def create_daily_power_readings(self):
 		self.power_readings['power'] = self.power_readings.apply(lambda row: self.power_formula(row['hours']), axis=1)
+		###self.power_readings['power'] = np.random.randint(0,5, size=len(self.power_readings))
 		#print(self.power_readings)
 		pass
-
 
 	def power_formula(self, hour):
 		'''
 		This should be re-written using processing DataFrames between power_config and the initialized power_readings
 		'''
 		if (hour >= 0) and (hour <= 5):
-			return float(500)
+			baseline = 500
 		elif (hour >= 6) and (hour <= 7):
-			return float(1000)
+			baseline = 1000
 		elif (hour >= 8) and (hour <= 15):
-			return float(500)
+			baseline = 500
 		elif (hour >= 16) and (hour <= 17):
-			return float(1500)
+			baseline = 1500
 		elif (hour >= 18) and (hour <= 19):
-			return float(3000)
+			baseline = 3000
 		elif (hour >= 20) and (hour <= 22):
-			return float(750)
+			baseline = 750
 		elif (hour == 23):
-			return float(500)
+			baseline = 500
+		power_value = float(np.random.randint(baseline*(1-TOLERANCE),baseline*(1+TOLERANCE)))
+		return(power_value)
 
 	def __str__(self):
 		to_string = 'Class Name = {} \nPower Config File = {}'.format(self.__class__.__name__, self.power_config_file)
